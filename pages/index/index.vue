6d1415973d6e388ddf89600a4d2c4091f522ca02
yangxing <2257580449@qq.com>
2023-09-04 18:36:38 +0800

    初始化首页，我的

---
+++ pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="index">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/user/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="user">
+		<view class="userHead">
+
+		</view>
+		<view class="option">
+
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "user",
+		data() {
+			return {}
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	.user {
+		background-color: #d1d1d1;
+
+		.userHead {
+			height: 10rem;
+			margin: 0 1rem;
+		}
+
+		.option {
+			height: 10rem;
+			margin: 1rem;
+			border-radius: .4rem;
+			background-color: #fff;
+		}
+	}
+</style>
